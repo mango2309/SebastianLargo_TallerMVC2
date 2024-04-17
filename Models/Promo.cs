77484fa66ec1bbf713888f980f0a9437e73d5ca8
mango2309 <sebastian.largo@udla.edu.ec>
2024-04-17 11:37:46 -0500

    Commit 1

---
+++ Models/Promo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SebastianLargo_TallerMVC2.Models
+{
+    public class Promo
+    {
+
+        [Key]
+        public int PromoID { get; set; }
+
+        [Required]
+        public string? Descripcion { get; set; }
+
+        [Required]
+        public DateTime FechaPromo {  get; set; }
+
+        public int BurgerId { get; set; }
+
+        public Burger? Burger { get; set; }
+
+
+
+
+
+    }
+}
