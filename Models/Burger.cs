a7091f7efd4420c3d3a23f62dbf84a3e938380ef
mango2309 <sebastian.largo@udla.edu.ec>
2024-04-17 11:48:13 -0500

    commit 2

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -10,5 +10,7 @@ namespace SebastianLargo_TallerMVC2.Models
         public bool WithCheese { get; set; }
         [Range(0.01, 9999.99)]
         public decimal Precio { get; set; }
+
+        public List<Promo> Promo { get; set; }
     }
 }
