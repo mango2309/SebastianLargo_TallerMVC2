883c473f1ee0dbe74407782b6e6ddbce8822fa5f
laboratorio <laboratorio@UPOAULA10302>
2024-04-17 11:27:39 -0500

    Agregar archivos de proyecto.

---
+++ Data/SebastianLargo_TallerMVC2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SebastianLargo_TallerMVC2.Models;
+
+namespace SebastianLargo_TallerMVC2.Data
+{
+    public class SebastianLargo_TallerMVC2Context : DbContext
+    {
+        public SebastianLargo_TallerMVC2Context (DbContextOptions<SebastianLargo_TallerMVC2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SebastianLargo_TallerMVC2.Models.Burger> Burger { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SebastianLargo_TallerMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
